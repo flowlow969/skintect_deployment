979ac08b5c8967f9f26cbf85dd058a637b228297
Florian Herrmann <ufh969@googlemail.com>
2022-12-18 23:13:03 +0100

    code kommentiert

--- app.py
+++ app.py
@@ -5,59 +5,66 @@ import os
 from werkzeug.utils import secure_filename
 from model import predict
 
-app = Flask(__name__)
+app = Flask(__name__)   # app initalisieren
 
-UPLOAD_FOLDER = 'static/uploads/'
+UPLOAD_FOLDER = 'static/uploads/'   # pfad zu uploads definieren
 
-app.secret_key = "secret key"
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 2048 * 2048
+app.secret_key = "secret key"   # verschlüsselung festlegen (muss bei deployment ausgetauscht werden)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER # pfad zu uploads zuweisen
+app.config['MAX_CONTENT_LENGTH'] = 16 * 2048 * 2048 # max größe festlegen
 
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif']) # erlaubte datentypen festlegen
 
 def allowed_file(filename):
+    """Funktion überptügt ob die Date in einem erlaubten Datentyp vorliegt"""
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/')
 def home():
+    """Funktion ruft Hauptseite auf"""
     return render_template('home.html')
 
 @app.route('/', methods=['POST'])
 def upload_image():
-    if 'file' not in request.files:
-        flash('No file part', category="error")
+    """Funktion regellt den upload von bildern"""
+    if 'file' not in request.files: # Datentyp prüfen
+        flash('No file part', category="error") # Fehler auf website ausgeben
         return redirect(request.url)
-    file = request.files['file']
-    if file.filename == '':
-        flash('No image selected for uploading', category="basic")
+    file = request.files['file']    #daten laden
+    if file.filename == '': # überprüfen ob ein Bild vorliegt
+        flash('No image selected for uploading', category="basic")   # Fehler auf website ausgeben
         return redirect(request.url)
     if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        detection,i = predict(filename)
+        filename = secure_filename(file.filename)   # dateinamen übertagen
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename)) # datei speichern
+        detection,i = predict(filename) # klassifizierung vornemen
         #print('upload_image filename: ' + filename)
-        if i == 1:
-            flash('Diese Hautveränderung ist wahrscheinlich  ein ' + detection + " und wahrscheinlich maligne.", category="error")
+        if i == 1:  # überprüfen ob die Hautveränderung Beligne oder maligne ist
+            flash('Diese Hautveränderung ist wahrscheinlich  ein ' + detection + " und wahrscheinlich maligne.", category="error") # Art der veränderung ausgeben (maligne=bössartig)
         else:
-            flash('Diese Hausveränderung ist wahrscheinlich  ein ' + detection + " und wahrscheinlich beligine.", category="success")
+            flash('Diese Hausveränderung ist wahrscheinlich  ein ' + detection + " und wahrscheinlich beligine.", category="success") # Art der veränderung ausgeben (Beligne=Gutartige)
         return render_template('home.html', filename=filename)
     else:
-        flash('Allowed image types are - png, jpg, jpeg, gif', category="error")
+        flash('Allowed image types are - png, jpg, jpeg, gif', category="error") # Fehler auf website ausgeben(falscher datentyp)
         return redirect(request.url)
 
 @app.route('/display/<filename>')
 def display_image(filename):
+    """Diese Funktion zeigt das Bild auf der Website an"""
     #print('display_image filename: ' + filename)
     return redirect(url_for('static', filename='uploads/' + filename), code=301)
 
 @app.route('/wir')
 def wir():
+    """Funktion ruft Über uns Seite auf"""
     return render_template("wir.html")
 
 @app.route('/HL')
 def hl():
+    """Funktion ruft dei Seite auf der die Hautveränderung erleutert werden auf"""
     return render_template("HL.html")
 
 if __name__ == "__main__":
+    """App laufenlassen auf Lockalhoste"""
     app.run( host='0.0.0.0')
--- model.py
+++ model.py
@@ -1,21 +1,25 @@
+#model.py
 import numpy as np
 from PIL import Image
 import tensorflow as tf
 
+# Typenden definieren
 CATEGORIES = ["Melanozytäre Nävi","Melanom","Gutartige keratoseähnliche Läsion","Basalzellkarzinom","Aktinische Keratose","Vaskuläre Läsionen","Dermatofibrom"]
-size = 100
-
+# Gefährlichkeit den Typen zuordnen
 lesion_danger = [0, 1, 0, 1, 1,  0, 0]
 
+# Vortrainertes Model laden
 model = tf.keras.models.load_model("256x3_128x3_64x3-CNN.model")
 
 
 def prepare(filepath):
-    IMG_SIZE = 100  # 50 in txt-based
-    new_array = np.asarray(Image.open(filepath).resize((size,size)))  # resize image to match model's expected sizing
-    return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 3)
+    """Diese Funktion bereitet die Daten für die Analyse vor."""
+    IMG_SIZE = 100  # Bildgröße festlegen
+    new_array = np.asarray(Image.open(filepath).resize((size,size)))  # Bild laden und auf die zuvor definierte Größe runterrechnen
+    return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 3) #Bild im passenden Format zurückgeben
 
 def predict(filename):
-    prediction = model.predict([prepare('static/uploads/'+ filename)])
-    print(prediction)  # will be a list in a list.
-    return CATEGORIES[int(prediction[0][0])], lesion_danger[int(prediction[0][0])]
+    """Diese Funktion ermittelt die klassifizierung des Bildes und gibt diese zurück"""
+    prediction = model.predict([prepare('static/uploads/'+ filename)]) # klassifizierung vornemen
+    print(prediction)  # klassifizierung ausgeben
+    return CATEGORIES[int(prediction[0][0])], lesion_danger[int(prediction[0][0])] # lesions typ und gefärlickeit zurückgeben
