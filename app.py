f71361bfba8fb54b9e99961e5025ee434c503b42
Nathalie Hilmer <nathalie.hilmer98@gmail.com>
2023-01-09 14:12:31 +0100

    Tippfehler korrigiert

--- app.py
+++ app.py
@@ -5,18 +5,18 @@ import os
 from werkzeug.utils import secure_filename
 from model import predict
 
-app = Flask(__name__)   # app initalisieren
+app = Flask(__name__)   # app initialisieren
 
-UPLOAD_FOLDER = 'static/uploads/'   # pfad zu uploads definieren
+UPLOAD_FOLDER = 'static/uploads/'   # Pfad zu uploads definieren
 
-app.secret_key = "secret key"   # verschlüsselung festlegen (muss bei deployment ausgetauscht werden)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER # pfad zu uploads zuweisen
-app.config['MAX_CONTENT_LENGTH'] = 16 * 2048 * 2048 # max größe festlegen
+app.secret_key = "secret key"   # Verschlüsselung festlegen (muss bei deployment ausgetauscht werden)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER # Pfad zu uploads zuweisen
+app.config['MAX_CONTENT_LENGTH'] = 16 * 2048 * 2048 # max Größe festlegen
 
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif']) # erlaubte datentypen festlegen
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif']) # erlaubte Datentypen festlegen
 
 def allowed_file(filename):
-    """Funktion überptügt ob die Date in einem erlaubten Datentyp vorliegt"""
+    """Funktion überptüft, ob die Datei in einem erlaubten Datentyp vorliegt"""
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
@@ -28,26 +28,26 @@ def home():
 
 @app.route('/', methods=['POST'])
 def upload_image():
-    """Funktion regellt den upload von bildern"""
+    """Funktion regelt den upload von Bildern"""
     if 'file' not in request.files: # Datentyp prüfen
-        flash('No file part', category="error") # Fehler auf website ausgeben
+        flash('No file part', category="error") # Fehler auf Website ausgeben
         return redirect(request.url)
-    file = request.files['file']    #daten laden
-    if file.filename == '': # überprüfen ob ein Bild vorliegt
-        flash('No image selected for uploading', category="basic")   # Fehler auf website ausgeben
+    file = request.files['file']    #Daten laden
+    if file.filename == '': # überprüfen, ob ein Bild vorliegt
+        flash('No image selected for uploading', category="basic")   # Fehler auf Website ausgeben
         return redirect(request.url)
     if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)   # dateinamen übertagen
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename)) # datei speichern
-        detection,i = predict(filename) # klassifizierung vornemen
+        filename = secure_filename(file.filename)   # Dateinamen übertragen
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename)) # Datei speichern
+        detection,i = predict(filename) # Klassifizierung vornehmen
         #print('upload_image filename: ' + filename)
-        if i == 1:  # überprüfen ob die Hautveränderung Beligne oder maligne ist
-            flash('Diese Hautveränderung ist wahrscheinlich  ein ' + detection + " und wahrscheinlich maligne.", category="error") # Art der veränderung ausgeben (maligne=bössartig)
+        if i == 1:  # überprüfen ob die Hautveränderung Benigne oder maligne ist
+            flash('Diese Hautveränderung ist wahrscheinlich  ein ' + detection + " und wahrscheinlich maligne.", category="error") # Art der Veränderung ausgeben (maligne=bösartig)
         else:
-            flash('Diese Hausveränderung ist wahrscheinlich  ein ' + detection + " und wahrscheinlich beligine.", category="success") # Art der veränderung ausgeben (Beligne=Gutartige)
+            flash('Diese Hausveränderung ist wahrscheinlich  ein ' + detection + " und wahrscheinlich benigne.", category="success") # Art der Veränderung ausgeben (Benigne=Gutartige)
         return render_template('home.html', filename=filename)
     else:
-        flash('Allowed image types are - png, jpg, jpeg, gif', category="error") # Fehler auf website ausgeben(falscher datentyp)
+        flash('Allowed image types are - png, jpg, jpeg, gif', category="error") # Fehler auf Website ausgeben(falscher Datentyp)
         return redirect(request.url)
 
 @app.route('/display/<filename>')
@@ -63,9 +63,9 @@ def wir():
 
 @app.route('/HL')
 def hl():
-    """Funktion ruft dei Seite auf der die Hautveränderung erleutert werden auf"""
+    """Funktion ruft die Seite auf, auf der die Hautveränderung erläutert werden"""
     return render_template("HL.html")
 
 if __name__ == "__main__":
-    """App laufenlassen auf Lockalhoste"""
+    """App laufenlassen auf Localhost"""
     app.run( host='0.0.0.0')
